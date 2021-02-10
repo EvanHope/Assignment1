222b4ae35002421f23549620d51ebe1522171d0c
Evan Hope <hopee1@tcnj.edu>
2021-02-10 15:25:10 -0500

    Room class and initialization method added

--- Hackathon.rb
+++ Hackathon.rb
@@ -1,1 +1,22 @@
-print "Hello World"
+class Room
+    attr_reader :building, :roomNum, :capacity, :computerAvail, :seatingAvail, :seatingType, :food, :priority, :roomType
+
+    def initialize(building, roomNum, capacity, computerAvail, seatingAvail, seatingType, food, priority, roomType)
+        @building = building
+        @roomNum = roomNum
+        @capacity = capacity
+        @computerAvail = computerAvail
+        @seatingAvail = seatingAvail
+        @seatingType = seatingType
+        @food = food
+        @priority = priority
+        @roomType = roomType
+    end
+end
+
+Room1 = Room.new("Armstrong", 12, 29, true, "desks", "Tiered", true, "Engineering", "Classroom")
+#For each line - 1 in file 1 create a new room obj and 
+#set all varibles before moving to next line 
+puts Room1.building
+puts Room1.roomNum
+puts Room1.capacity
