736b01be22b40edb10f10ab073dd3eec796d3bfd
Evan Hope <hopee1@tcnj.edu>
2021-02-12 14:17:11 -0500

    Basic structer added

--- Hackathon.rb
+++ Hackathon.rb
@@ -1,8 +1,78 @@
+#------------------------------------------------------------------------------------------------------------
+#Evan Hope
+#2/15/21
+#Assignment 1: Application Development
+#Program which user inputs 2 csv files containing information about rooms and the times the rooms are booked.
+#The user also enters information about an event they would like to host. The program then parses the 2 csv 
+#files and compares the file and input from user to find suitible rooms and times for specified event.
+#------------------------------------------------------------------------------------------------------------
 require 'csv'
 
 
+#------------------------------------------------------------------------------------------------------------
+#User Input collection
+#Gets name of csv files and formats into parsed table also gets required information about event
+#
+#Varribles: roomTable, scheduleTable, roomCsvLength, scheduleCsvLength, inputDate, inputStartTime,
+#inputDuration, inputNumOfAttendees
+#------------------------------------------------------------------------------------------------------------
+j = 1
+while j == 1 #getting user input for room information file
+    puts "Enter room information filename (including .csv): "
+    file = gets.chomp
+    if File.exist?(file)#if file exists, continue
+        roomTable = CSV.parse(File.read(file), headers: true)
+        j = 0
+        break
+    else #if file doesnt exist, loop back
+        puts "Incorrect file, please try again \n"
+    end
+end
 
+j = 1
+while j == 1 #getting user input for schedule file
+    puts "Enter schedule filename (including .csv): "
+    file = gets.chomp
+    if File.exist?(file)#if file exists, continue
+        scheduleTable = CSV.parse(File.read(file), headers: true)
+        j = 0
+        break
+    else #if file doesnt exist, loop back
+        puts "Incorrect file, please try again \n"
+    end
+end
 
+roomCsvLength = roomTable.size
+scheduleCsvLength = scheduleTable.size
+
+#j = 1
+#while j == 1
+    puts "Enter date of event (Format: yyyy-mm-dd)"
+    #TODO:Make sure user enters correct format
+    inputDate = gets.chomp
+    #if inputDate is valid
+        #j = 0
+        #break
+    #else
+        #puts incorrect format, please try again
+    #end
+
+    puts "Enter start time of event (Format: hh.mm AM/PM)"
+    inputStartTime = gets.chomp
+
+    puts "Enter duration of event (Format: hh.mm)"
+    inputDuration = gets.chomp
+
+    puts "Enter number of attendees"
+    inputNumOfAttendees = gets.chomp
+
+#------------------------------------------------------------------------------------------------------------
+#Room Class Definition
+#Room class Varibles: building, roomNum, capacity, computerAvail, seatingAvail, seatingType, food, priority,
+#roomType
+#
+#------------------------------------------------------------------------------------------------------------
+#Room Object
 class Room
     attr_reader :building, :roomNum, :capacity, :computerAvail, :seatingAvail, :seatingType, :food, :priority, :roomType
 
@@ -17,16 +87,47 @@ class Room
         @priority = priority
         @roomType = roomType
     end
+#    def setSchedule(index, availiblity, )
+#        availiblity[index] = availiblity
+#    end
 end
 
+#------------------------------------------------------------------------------------------------------------
+#Room Object Creation
+#Creates a room object for each room in roomTable and assigns its varibles using roomTable data
+#
+#------------------------------------------------------------------------------------------------------------
+#While loop to create a Room obj for each row in rooms.csv
+roomsArray = []
+i = 0
+while i < roomCsvLength
+    roomsArray[i] = Room.new(roomTable[i]["Building"], roomTable[i]["Room"], roomTable[i]["Capacity"], roomTable[i]["Computers Available"], roomTable[i]["Seating Available"], roomTable[i]["Seating Type"], roomTable[i]["Food Allowed"], roomTable[i]["Priority"], roomTable[i]["Room Type"])
+#    j = 0
+#    a = 0
+#    while j < scheduleCsvLength
+#        if roomsArray[i].building == scheduleTable[j]["Building"] && roomsArray[i].roomNum == scheduleTable[j]["Room"]
+#            roomsArray[i].setSchedule(a, scheduleTable[j]["Available"], )
+#    end
+    i+=1
+end
 
-table = CSV.parse(File.read("rooms.csv"), headers: true)
-#puts table[0]["Building"]
-numOfLines = table.size
+puts roomsArray[1].roomNum
+puts roomsArray[2].roomNum
 
-#TO DO: add a for loop for to create a Room obj for each row in rooms.csv
-Room1 = Room.new(table[0]["Building"], table[0]["Room"], table[0]["Capacity"], table[0]["Computers Available"], table[0]["Seating Available"], table[0]["Seating Type"], table[0]["Food Allowed"], table[0]["Priority"], table[0]["Room Type"])
+#------------------------------------------------------------------------------------------------------------
+#Seperates speciality rooms into their own arrays so certain requirements can be satisfied easily
+#------------------------------------------------------------------------------------------------------------
 
-puts Room1.building
-puts Room1.roomNum
-puts Room1.capacity
+i = 0
+fullCapacityRoomsArray = []
+foodRoomsArray = []
+computerRoomsArray = []
+while i < roomsArray.length
+    if roomsArray[i].capacity >= inputNumOfAttendees && (roomsArray[i].roomType == "Conference Room" || roomsArray[i].roomType == "Event Center")
+        fullCapacityRoomsArray.append(roomsArray[i])
+    end
+    if roomsArray[i].food == "yes"
+        foodRoomsArray.append(roomsArray[i])
+    end
+    i+=1
+end
